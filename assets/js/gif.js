88d26313ab1a162b6578b5e75daedf2e8172c9bd
Alex Rosenkranz <alexrosenkranz@Alexs-iMac.local>
2016-12-19 11:10:45 -0500

    styled page up a little more

--- assets/js/gif.js
+++ assets/js/gif.js
@@ -1,33 +1,28 @@
 $(document).ready(function() {
 
   //Create Variables
-    // Array for buttons
+    // Array for topics
     // URL for API call
     // Variable for clicked button's value to be passed through the API
 
-  var buttons = ['Gob Bluth','Ron Swanson','Community','Archer', 'WWE'];
+  var topics = ['Gob Bluth','Ron Swanson','Community','Archer', 'WWE'];
   var url = 'https://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&limit=20&q=';
   var queryValue;
 
-  // Create buttons from array
-  function createButtons() {
+  // Create topics from array
+  function createTopics() {
 
     // Start list from scratch to prevent any duplicates
     $(".buttonArea").empty();
     
     // Loops through array
-    for (var i = 0; i < buttons.length; i++) {
-
-      // Then dynamicaly generates buttons for each movie in the array
-      // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+    for (var i = 0; i < topics.length; i++) {
       var a = $("<button>");
-      // Adds a class of movie to our button
+      // Adds a class of gif to our button
       a.addClass("gifButton btn btn-lg btn-default btn-block");
       // Added a data-attribute
-      a.attr("data-name", buttons[i]);
-      // Provided the initial button text
-      a.append(buttons[i]);
-      // Added the button to the buttons-view div
+      a.attr("data-name", topics[i]);
+      a.append(topics[i]);
       $(".buttonArea").append(a);
     }
   };
@@ -40,24 +35,27 @@ $(document).ready(function() {
   function gifDisplay() {
 
     $('.results').empty();
+
+    // Add name of Gif list on top
+    $('.results').append('<h2 class="result_title">Now Playing: <span>' + $(this).data('name') + ' Gifs!</span></h2>' );
+
+    // removes blank spaces in names and puts %20 in url
     url = url + escape($(this).data('name'));
     console.log(url);
-    // Creates AJAX call for the specific movie button being clicked
+    // Creates AJAX call for the specific button being clicked
     $.ajax({
       url: url,
       method: "GET"
     }).done(function(response) {
       console.log(response);
       var gif = response.data;
-      for (var i = 0; i < 20; i++) {
+      for (var i = 0; i < 10; i++) {
         
-        // Creates a div to hold the gif
+        // Creates a div to hold the gif and appends it to .results class on page (in right-column)
         var gifDiv = $('<div>');
         gifDiv.addClass('gif col-md-4 col-xs-12 col-sm-4');
-
         gifDiv.append('<img class="gifImg" src='+gif[i].images.fixed_height_still.url + ' data-still=' + gif[i].images.fixed_height_still.url + ' data-animate=' + gif[i].images.fixed_height.url + ' data-state="still"/>');
-        gifDiv.append('<h3 class="label label-default">Rated: '+gif[i].rating + '</h3>');
-
+        gifDiv.append('<span class="label label-default">Rated: '+gif[i].rating + '</span>');
         $('.results').append(gifDiv);
       }
 
@@ -69,19 +67,28 @@ $(document).ready(function() {
       }
 
     });
-
+    // Reset URL Value after everything loads
     url = 'https://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&limit=20&q=';
   }
 
   // Form input click
     // Push value of input box into button array
     // Prevent default function from happening (reload)
-    // Refresh buttons
+    // Refresh topics
   $('.submit').on('click',function(event) {
     event.preventDefault();
-    var newButton = $('#userInput').val().trim();
-    buttons.push(newButton);
-    createButtons();
+    
+
+    if ($('#userInput').val() != '') {
+      var newButton = $('#userInput').val().trim();
+      topics.push(newButton);
+      createTopics();
+    }
+    else {
+      alert("You didn't enter anything, dum-dum!");
+      createTopics();
+    }
+    $('#userInput').val("");
   });
 
 
@@ -106,7 +113,7 @@ $(document).ready(function() {
   });
 
 
- createButtons();
+ createTopics();
 
 
 });
